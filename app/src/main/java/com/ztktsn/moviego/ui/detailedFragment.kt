3cc548ee4bcfe8498fb0891d4282683c6b4d9c80
Your Name <nightblue200416@gmail.com>
2024-05-02 16:56:00 +0500

    detailed page fragment added
    detailed page fragment added but its empty

---
+++ app/src/main/java/com/ztktsn/moviego/ui/detailedFragment.kt
@@ -0,0 +1,27 @@
+package com.ztktsn.moviego.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class detailedFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+}
