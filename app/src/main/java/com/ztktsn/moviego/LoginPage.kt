c973d2d7791f169b10718253afa543a2aa931538
Your Name <nightblue200416@gmail.com>
2024-05-12 12:42:14 +0500

    starting activity changed

--- app/src/main/java/com/ztktsn/moviego/LoginPage.kt
+++ app/src/main/java/com/ztktsn/moviego/LoginPage.kt
@@ -1,11 +1,32 @@
 package com.ztktsn.moviego
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ztktsn.moviego.databinding.ActivityLoginPageBinding
 
 class LoginPage : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        binding = ActivityLoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding = ActivityLoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//
+//        binding.createAcc.setOnClickListener {
+//      s      val intent = Intent(this, createAccount::class.java)
+//            startActivity(intent)
+//
+//
+//        }
     }
 }
